26fd56bf6e6fbc65f73eb9a435296d4516bef8ec
gdpatil39 <gdpatil39@gmail.com>
2024-12-19 19:41:08 +0530

    upto controller

---
+++ blog-app-apis-2/src/main/java/com/blogger/payloads/ApiResponse.java
@@ -0,0 +1,19 @@
+package com.blogger.payloads;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ApiResponse {
+
+	
+private String massage;
+	
+	private boolean success;
+}
