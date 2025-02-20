021f8dde19315a3a1109944251848e2db62a7598
gdpatil39 <gdpatil39@gmail.com>
2025-02-20 14:08:55 +0530

    26 video completed

--- blog-app-apis-2/src/main/java/com/blogger/controllers/PostController.java
+++ blog-app-apis-2/src/main/java/com/blogger/controllers/PostController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import com.blogger.config.AppConstants;
 import com.blogger.entity.Post;
 import com.blogger.payloads.ApiResponse;
 import com.blogger.payloads.PostDto;
@@ -70,10 +71,10 @@ public class PostController {
 	//get all post
 	@GetMapping("/posts")
 	public ResponseEntity<PostResponse> getAllPost(
-			@RequestParam(value= "pageNumber",defaultValue = "0",required=false)Integer pageNumber ,
-			@RequestParam(value= "pageSize",defaultValue = "10",required=false) Integer pageSize ,
-			@RequestParam(value= "sortBy",defaultValue = "postId",required=false) String sortBy,
-			@RequestParam(value= "sortDir",defaultValue = "asc",required=false) String sortDir
+			@RequestParam(value= "pageNumber",defaultValue = AppConstants.PAGE_NUMBER,required=false)Integer pageNumber ,
+			@RequestParam(value= "pageSize",defaultValue = AppConstants.PAGE_SIZE,required=false) Integer pageSize ,
+			@RequestParam(value= "sortBy",defaultValue = AppConstants.SORT_BY,required=false) String sortBy,
+			@RequestParam(value= "sortDir",defaultValue = AppConstants.SORT_DIR,required=false) String sortDir
 
 			){
 		 PostResponse postResponse = this.postService.getAllPost(pageNumber,pageSize,sortBy,sortDir);
